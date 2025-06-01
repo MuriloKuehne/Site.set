47c68bebad7ab5c02ecac109c3f0fdc66fd3d028
Murilo <murkuehne@gmail.com>
2025-06-01 14:06:36 -0300

    Improving the SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,29 @@
+import type { Metadata } from "next"
 import { LandingPage } from "@/templates/landing-page"
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description:
+    "Venda seus produtos como afiliado em um único lugar, com Site.Set",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description:
+      "Venda seus produtos como afiliado em um único lugar, com Site.Set",
+    url: "https://https://site-set-two.vercel.app/og-image.png",
+    siteName: "Site.Set",
+    images: [
+      {
+        url: "https://site.set/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Site.Set - Venda seus produtos como afiliado em um único lugar",
+      },
+    ],
+    locale: "pt-BR",
+    type: "website",
+  },
+}
 
 export default function HomePage() {
   return (
