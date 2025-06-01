c3c328acbece6289674ce894f2abf71cff54b0a9
Murilo <murkuehne@gmail.com>
2025-06-01 14:44:46 -0300

    Added the last SEO improvements

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,44 @@
 import NotFound from "@/app/not-found"
 import PostPage from "@/pages/blog-page/[slug]"
 import { allPosts } from "contentlayer/generated"
+import { title } from "process"
 
 type BlogPostPageProps = {
   params: Promise<{
     slug: string
   }>
+}
+export async function generateMetadata({ params }: BlogPostPageProps) {
+  const { slug } = await params
+  const post = allPosts.find((post) => post.slug === slug)
+  if (!post) {
+    return {
+      title: "Post não encontrado",
+      description: "O post requesitado não existe.",
+    }
+  }
+  return {
+    title: post.title,
+    description: post.description,
+    author: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      url: `https://site-set-two.vercel.app/blog/${post.slug}`,
+      siteName: "Site.Set",
+      images: [
+        {
+          url: post.image,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+      locale: "pt-BR",
+      type: "article",
+    },
+  }
 }
 
 export const revalidate = 60 // Revalidate every 60 seconds
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,29 @@
+import { Metadata } from "next"
 import { BlogList } from "@/templates/blog"
 import { allPosts } from "contentlayer/generated"
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas para impulsionar suas vendas como afiliado com Site.Set",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description:
+      "Venda seus produtos como afiliado em um único lugar, com Site.Set",
+    url: "https://site-set-two.vercel.app/og-image.png",
+    siteName: "Site.Set",
+    images: [
+      {
+        url: "https://site-set-two.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Blog",
+      },
+    ],
+    locale: "pt-BR",
+    type: "website",
+  },
+}
 
 export default function BlogListPage() {
   // acontece exclusivamente no lado do servidor
